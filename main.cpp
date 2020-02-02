ec13ea94ccc5ef50ec3cf52fc8031767b01cbdfb
Catalin <ancutei.catalin@gmail.com>
2020-02-02 18:01:08 +0200

    Fereastra SDL

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
+    bool isRunning=true;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_JPG);
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800,600,0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Event e;
+
+    while(isRunning)
+    {
+        if(SDL_PollEvent(&e))
+            if(e.type == SDL_QUIT)
+                isRunning=false;
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+
     return 0;
 }
