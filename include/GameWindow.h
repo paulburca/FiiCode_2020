559db1385dccd46cc9cf2f63fa1d4fba60827896
Catalin <ancutei.catalin@gmail.com>
2020-02-15 19:32:47 +0200

    Clasa GameWindow

---
+++ include/GameWindow.h
@@ -0,0 +1,16 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
+
+
+class GameWindow
+{
+    public:
+        GameWindow();
+        virtual ~GameWindow();
+
+    protected:
+
+    private:
+};
+
+#endif // GAMEWINDOW_H
---
+++ src/GameWindow.cpp
@@ -0,0 +1,11 @@
+#include "GameWindow.h"
+
+GameWindow::GameWindow()
+{
+    //ctor
+}
+
+GameWindow::~GameWindow()
+{
+    //dtor
+}
